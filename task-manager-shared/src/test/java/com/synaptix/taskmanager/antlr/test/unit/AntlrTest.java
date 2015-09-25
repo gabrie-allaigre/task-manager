e1cb505d973582d619f612fc84e771fb09f333f2
Gabriel Allaigre <gabriel.allaigre@synaptix-labs.com>
2015-09-25 17:41:04 +0200

    Add unit

--- task-manager-shared/src/test/java/com/synaptix/taskmanager/antlr/test/unit/AntlrTest.java
+++ task-manager-shared/src/test/java/com/synaptix/taskmanager/antlr/test/unit/AntlrTest.java
@@ -3,18 +3,12 @@ package com.synaptix.taskmanager.antlr.test.unit;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.synaptix.taskmanager.antlr.*;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.junit.Assert;
 import org.junit.Test;
-
-import com.synaptix.taskmanager.antlr.AbstractGraphNode;
-import com.synaptix.taskmanager.antlr.EvalGraphCalcVisitor;
-import com.synaptix.taskmanager.antlr.GraphCalcLexer;
-import com.synaptix.taskmanager.antlr.GraphCalcParser;
-import com.synaptix.taskmanager.antlr.IdGraphNode;
-import com.synaptix.taskmanager.antlr.ThrowingErrorListener;
 
 public class AntlrTest {
 
@@ -30,7 +24,7 @@ public class AntlrTest {
 			parser.addErrorListener(ThrowingErrorListener.INSTANCE);
 			AbstractGraphNode graphNode = new EvalGraphCalcVisitor().visit(parser.compile());
 
-			Assert.assertTrue(true);
+			Assert.assertEquals("A",toString(graphNode));
 		} catch (ParseCancellationException t) {
 			Assert.assertTrue(false);
 		}
@@ -66,7 +60,7 @@ public class AntlrTest {
 			parser.addErrorListener(ThrowingErrorListener.INSTANCE);
 			AbstractGraphNode graphNode = new EvalGraphCalcVisitor().visit(parser.compile());
 
-			Assert.assertTrue(true);
+			Assert.assertEquals("(A=>B)", toString(graphNode));
 		} catch (Throwable t) {
 			Assert.assertTrue(false);
 		}
@@ -102,7 +96,7 @@ public class AntlrTest {
 			parser.addErrorListener(ThrowingErrorListener.INSTANCE);
 			AbstractGraphNode graphNode = new EvalGraphCalcVisitor().visit(parser.compile());
 
-			Assert.assertTrue(true);
+			Assert.assertEquals("(A=>(B,(C=>D)))", toString(graphNode));
 		} catch (Throwable t) {
 			Assert.assertTrue(false);
 		}
@@ -120,7 +114,7 @@ public class AntlrTest {
 			parser.addErrorListener(ThrowingErrorListener.INSTANCE);
 			AbstractGraphNode graphNode = new EvalGraphCalcVisitor().visit(parser.compile());
 
-			Assert.assertTrue(true);
+			Assert.assertEquals("((A,E,F)=>(B,(C=>D)))", toString(graphNode));
 		} catch (Throwable t) {
 			Assert.assertTrue(false);
 		}
@@ -138,7 +132,7 @@ public class AntlrTest {
 			parser.addErrorListener(ThrowingErrorListener.INSTANCE);
 			AbstractGraphNode graphNode = new EvalGraphCalcVisitor().visit(parser.compile());
 
-			Assert.assertTrue(true);
+			Assert.assertEquals("A123_123-az", toString(graphNode));
 		} catch (Throwable t) {
 			Assert.assertTrue(false);
 		}
@@ -156,7 +150,7 @@ public class AntlrTest {
 			parser.addErrorListener(ThrowingErrorListener.INSTANCE);
 			AbstractGraphNode graphNode = new EvalGraphCalcVisitor().visit(parser.compile());
 
-			Assert.assertTrue(true);
+			Assert.assertEquals("A", toString(graphNode));
 		} catch (Throwable t) {
 			Assert.assertTrue(false);
 		}
@@ -180,10 +174,32 @@ public class AntlrTest {
 		}
 	}
 
-	private static List<Class<?>> extract(AbstractGraphNode node) {
-		List<Class<?>> res = new ArrayList<Class<?>>();
+	private static String toString(AbstractGraphNode node) {
 		if (node instanceof IdGraphNode) {
+			return ((IdGraphNode) node).getId();
+		} else if (node instanceof ParallelGraphNode) {
+			ParallelGraphNode pgn = (ParallelGraphNode) node;
+
+			StringBuilder sb = new StringBuilder("(");
+			boolean first = true;
+			for (AbstractGraphNode child : pgn.getNodes()) {
+				if (first) {
+					first = false;
+				} else {
+					sb.append(",");
+				}
+				sb.append(toString(child));
+			}
+			sb.append(")");
+			return sb.toString();
+		} else if (node instanceof NextGraphNode) {
+			NextGraphNode ngn = (NextGraphNode) node;
+
+			String firstCr = toString(ngn.getFirstNode());
+			String nextCr = toString(ngn.getNextNode());
+
+			return "(" + firstCr + "=>" + nextCr + ")";
 		}
-		return res;
+		return "";
 	}
 }

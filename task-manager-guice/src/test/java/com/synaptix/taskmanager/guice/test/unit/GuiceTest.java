ff6ef46caa752dad562ec4d8c1f33fbf0bc09f23
Gabriel Allaigre <gabriel.allaigre@synaptix-labs.com>
2015-10-08 14:48:56 +0200

    guice

--- task-manager-guice/src/test/java/com/synaptix/taskmanager/guice/test/unit/GuiceTest.java
+++ task-manager-guice/src/test/java/com/synaptix/taskmanager/guice/test/unit/GuiceTest.java
@@ -1,11 +1,19 @@
 package com.synaptix.taskmanager.guice.test.unit;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.MapBinder;
 import org.junit.Test;
 
 public class GuiceTest {
 
 	@Test
 	public void test1() {
+		new AbstractModule() {
 
+			@Override
+			protected void configure() {
+				MapBinder.newMapBinder(binder(),String.class,String.class).addBinding("toto").toInstance("rien");
+			}
+		};
 	}
 }
